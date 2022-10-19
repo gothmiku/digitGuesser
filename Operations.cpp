5c01fb1e2210c9fc4cee6b6ed8a4aa1082332c9c
gothmiku <berkan10001@gmail.com>
2022-10-19 22:38:42 +0300

    Fixed issues
    Fixed the header issue
    Expanded on the header function prototypes
    Added in a few operations

--- Operations.cpp
+++ Operations.cpp
@@ -17,8 +17,39 @@ Matrix add(Matrix A, Matrix B){
     if(dimensionCheck(A,B)){
         for(int m=0;m<A.matrixGetRow();m++){
             for(int n=0;n<A.matrixGetCol();n++){
-
+                result->data[m][n]=A.matrixGetData(m,n)+B.matrixGetData(m,n);
             }
         }
     }
+}
+
+Matrix substract(Matrix A, Matrix B){
+    Matrix* result = new Matrix(A.matrixGetRow(),A.matrixGetCol(),createNullArray(A.matrixGetRow(),A.matrixGetCol()));
+    if(dimensionCheck(A,B)){
+        for(int m=0;m<A.matrixGetRow();m++){
+            for(int n=0;n<A.matrixGetCol();n++){
+                result->data[m][n]=A.matrixGetData(m,n)-B.matrixGetData(m,n);
+            }
+        }
+    }
+}
+
+Matrix multiply(Matrix A,Matrix B){
+    //i forgot how we multiplied
+}
+
+Matrix transpose(Matrix A){
+    //i forgot how to transpose
+}
+
+Matrix addScalar(Matrix A,const T scaValue){
+    //will do
+}
+
+Matrix scale(Matrix A,const T scaValue){
+    //will do
+}
+
+Matrix dotProd(Matrix A,Matrix B){
+    
 }
--- headers/Operations.h
+++ headers/Operations.h
@@ -4,3 +4,8 @@
 
 bool dimensionCheck(Matrix A,Matrix B);
 Matrix add(Matrix A, Matrix B);
+Matrix substract(Matrix A, Matrix B);
+Matrix multiply(Matrix A,Matrix B);
+Matrix transpose(Matrix A);
+Matrix addScalar(Matrix A,const T scaValue);
+Matrix scale(Matrix A,const T scaValue);
